9ad088d6c3a0b4e53adbc03a0141d9a9dba1f63b
Kanistra006 <167027245+Kanistra006@users.noreply.github.com>
2024-04-28 21:56:34 +0300

    Движение игрока

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] int _speed;
+    public float boundaryLeft = -8.0f;
+    public float boundaryRight = 8.0f;
     void Start()
     {
         
@@ -13,9 +16,20 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        Mathf.Clamp(transform.position.x, boundaryLeft, boundaryRight);
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += new Vector3(1f, 0);
+            transform.position += new Vector3(1f, 0) * _speed * Time.deltaTime;
         }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1f, 0) * _speed * Time.deltaTime;
+        }
+        
+    transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, boundaryLeft, boundaryRight),
+            transform.position.y,
+            transform.position.z);
+
     }
 }
