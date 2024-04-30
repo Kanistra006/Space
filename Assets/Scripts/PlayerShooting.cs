cf512fc282a1114fe9339b7406743c6b651cddd7
Kanistra006 <167027245+Kanistra006@users.noreply.github.com>
2024-05-01 01:32:20 +0300

    Стрельба перса и физика для врагов и перса

---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawnPoint;
+    float bulletSpeed = 20.0f;
+    float shootingRate = 0.2f;
+    float shootingCooldown;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        shootingCooldown -= Time.deltaTime;
+
+        if (shootingCooldown <= 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+            shootingCooldown = shootingRate;
+        }
+    }
+
+    void Shoot()
+    {
+        // Создаёт копию префаба снаряда в заданной позиции и с заданной ориентацией
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
+
+        // Присваивает снаряду скорость в направлении, зависящем от ориентации объекта, к которому прикреплён скрипт
+        bullet.GetComponent<Rigidbody2D>().velocity = transform.up * bulletSpeed;
+    }
+}
